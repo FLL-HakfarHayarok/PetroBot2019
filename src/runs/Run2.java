5668871f3c99eef188149fdaeee8f909e349938e
SoftwareGoodPC <greenblitz.programmers@gmail.com>
2019-01-13 10:18:04 +0200

    Began work on run 2

--- src/runs/Run2.java
+++ src/runs/Run2.java
@@ -1,16 +1,30 @@
 package runs;
 
 import robotUtils.RobotRun;
+import robotUtils.RobotStructure;
 import util.Chassis;
 import util.Direction;
 import util.GyroSensor;
+import util.Wait;
 
 public class Run2 extends RobotRun {
 
 	@Override
 	public void runInstructions() {
 		GyroSensor.resetGyro();
-		GyroSensor.gyroFollower(30, 0, 500, Chassis.distToDeg(80), Direction.FORWARD, false);
+		GyroSensor.gyroFollower(30, 0, 500, Chassis.distToDeg(70), Direction.FORWARD, false);
+		GyroSensor.gyroFollower(30, 0, 200, Chassis.distToDeg(10), Direction.FORWARD, false);
+		RobotStructure.getInstance().leftMotorReg.setSpeed(800);
+		RobotStructure.getInstance().leftMotorReg.forward();
+		Wait.waitForSeconds(1);
+		RobotStructure.getInstance().rightMotorReg.setSpeed(800);
+		Chassis.drive(Direction.FORWARD);
+		Wait.waitForSeconds(2);
+		Chassis.drive(Direction.BACKWARD);
+		Wait.waitForSeconds(0.75);
+		Chassis.drive(Direction.FORWARD);
+		Wait.waitForSeconds(0.75);
+		
 	}
 
 }
